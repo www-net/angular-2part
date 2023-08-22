b9df6e8e16e9ac7b813c96ec58c36637e7ccace5
www-net <78052177+www-net@users.noreply.github.com>
2023-08-22 13:45:03 +0300

    ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,4 @@ import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angu
 })
 export class AppComponent {
   title = 'angular-view-children';
-  fullName: string = '';
-
-  @ViewChildren('inputEl', {})
-  inputElements!: QueryList<ElementRef>;
-
-ngOnInit(){
-  this.inputElements.forEach((el) => {
-    console.log(el.nativeElement.value);
-  })
-}
-
-  show(){
-    let name = ''
-    this.inputElements.forEach((el) => {
-      name += el.nativeElement.value + ' ';
-    })
-  this.fullName = name.trim();
-  }
-
 }
