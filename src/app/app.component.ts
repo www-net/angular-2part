c02907cd839331b2f4c2a2d6c6d92bd4d33cd9ec
www-net <78052177+www-net@users.noreply.github.com>
2023-08-22 12:48:43 +0300

    use ViewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-view-children';
+  fullName: string = '';
+
+  @ViewChildren('inputEl', {})
+  inputElements!: QueryList<ElementRef>;
+
+ngOnInit(){
+  this.inputElements.forEach((el) => {
+    console.log(el.nativeElement.value);
+  })
+}
+
+  show(){
+    let name = ''
+    this.inputElements.forEach((el) => {
+      name += el.nativeElement.value + ' ';
+    })
+  this.fullName = name.trim();
+  }
+
 }
